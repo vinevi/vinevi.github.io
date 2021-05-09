20d98b68c127b7709c34666f5229ef56559dff09
vinevi <vineviart@gmail.com>
2021-05-09 13:44:40 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2dbe05b85d698e6661a8.js"
+    "url": "webpack-runtime-b304593fc2ffeeb5463d.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "de76a7d21e3204526cc92aafe64fe58d"
+    "revision": "74230ae191ab22dbbd2389a5663b6348"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
